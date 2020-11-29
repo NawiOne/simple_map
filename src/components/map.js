3a53453acdac26174b183bb787a1722411c73eec
shadwoe <rudinnawi@gmail.com>
2020-11-29 16:26:41 +0700

    commit

--- src/components/map.js
+++ src/components/map.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, View} from 'react-native';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import GetLocation from 'react-native-get-location';
 
 import {useSelector, useDispatch} from 'react-redux';
 import {getCoordinatCreator} from '../redux/action/coordinat';
@@ -12,8 +13,35 @@ const Map = () => {
     latitude: -6.884831231113922,
     longitude: 108.7806924059987,
   });
+  const [curentLoc, setLoc] = useState({
+    latitude: -7.198528,
+    longitude: 109.1576702,
+  });
 
   const dispatch = useDispatch();
+
+  const getCurentLoc = () => {
+    GetLocation.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 15000,
+    })
+      .then((location) => {
+        setLoc({
+          ...curentLoc,
+          latitude: location.latitude,
+          longitude: location.longitude,
+        });
+      })
+      .catch((error) => {
+        const {code, message} = error;
+        console.warn(code, message);
+      });
+  };
+
+  useEffect(() => {
+    getCurentLoc();
+    console.log(curentLoc);
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -21,10 +49,10 @@ const Map = () => {
         provider={PROVIDER_GOOGLE}
         style={styles.map}
         initialRegion={{
-          latitude: -6.884831231113922,
-          longitude: 108.7806924059987,
-          latitudeDelta: 37.326806265848965,
-          longitudeDelta: 22.384255975484876,
+          latitude: curentLoc.latitude,
+          longitude: curentLoc.longitude,
+          latitudeDelta: 3,
+          longitudeDelta: 3,
         }}
         onPress={(e) => {
           setRegion({
